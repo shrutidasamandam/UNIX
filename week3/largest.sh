62bdf07fcc88a7389fa684b377de5785d576a2cd
shrutidasamandam <44689940+shrutidasamandam@users.noreply.github.com>
2023-01-10 21:41:09 +0530

    Create largest.sh

---
+++ week3/largest.sh
@@ -0,0 +1,11 @@
+echo "The 3 numbers : \c"
+echo "$1 $2 $3"
+if [ $1 -ge $2 ] && [ $1 -ge $3 ]
+then
+echo "$1 is the largest";
+elif [ $2 -ge $1 ] && [ $2 -ge $3 ]
+then
+echo "$2 is the largest";
+else
+echo "$3 is the largest";
+fi
