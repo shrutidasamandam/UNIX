45cd9bcf8ee3afbf1591bd27d4e57beb259c95ba
shrutidasamandam <44689940+shrutidasamandam@users.noreply.github.com>
2023-01-10 21:24:13 +0530

    Create countnumofvowels.sh

---
+++ week7/countnumofvowels.sh
@@ -0,0 +1,14 @@
+echo "Enter the string"
+read str
+counter=0
+i=1
+len=${#str}
+while [ $i -le $len ];do
+ch=$(echo $str | cut -c "$i")
+if [ $ch = "a" -o $ch = "e" -o $ch = "i" -o $ch = "o" -o $ch =  "u" ]
+then
+counter=`expr $counter+1|bc`
+fi
+i=`expr $i+1|bc`
+done
+echo $counter
