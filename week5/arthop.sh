00e1dd994c6b3bafd8b61f37b455332883d23be4
shrutidasamandam <44689940+shrutidasamandam@users.noreply.github.com>
2023-01-10 21:12:34 +0530

    Create arthop.sh

---
+++ week5/arthop.sh
@@ -0,0 +1,13 @@
+echo "two numbers:"
+read a
+read b
+echo "1-->add\n2-->subtract\n3-->multiply\n4-->divde\n\n"
+read ch
+echo "answer:\n"
+case "$ch" in
+1) echo `expr $a+$b|bc`;;
+2) echo `expr $a-$b|bc`;;
+3) echo `expr $a*$b|bc`;;
+4) echo `expr $a/$b|bc`;;
+*) echo "invalid choice";;
+esac
