e85a636d225aeae197a6b1d524453455ef8d300f
shrutidasamandam <44689940+shrutidasamandam@users.noreply.github.com>
2023-01-10 21:02:36 +0530

    Create grep.sh

---
+++ week1/grep.sh
@@ -0,0 +1,9 @@
+if grep $1 emp.lst
+then
+echo "exit status:$?";
+echo "pattern $1 found";
+echo "number of args passed:\t$#";
+echo "All the passed arguments are:\t$*";
+else
+echo "pattern not found";
+fi
